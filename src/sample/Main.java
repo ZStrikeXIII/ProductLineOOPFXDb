a8b5f820f904227440724e4ac802fdd036fc2e8e
ZStrikeXIII <jacksonturner1999@gmail.com>
2019-09-16 19:14:18 -0400

    First Update:      Added a myriad of new code, and added multuiple plugins to help with styling.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,29 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * @author Jackson Turner
+ */
+
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+  /**
+   * The "start" method is the starting point of a JavaFX program. This method sets the title.
+   *
+   * @param primaryStage This is the Primary Stage
+   * @throws Exception These are any problems that occur within the code
+   * @brief
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("Hello World");
+    primaryStage.setScene(new Scene(root, 300, 275));
+    primaryStage.show();
+  }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }

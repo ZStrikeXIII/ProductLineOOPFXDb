c981f26ce7b37b0d67cbb7bc9ac873ffbb9556fd
ZStrikeXIII <jacksonturner1999@gmail.com>
2019-09-28 13:03:32 -0400

    5.0 Update:     Added Event Handlers to Buttons.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,33 @@
 package sample;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.event.ActionEvent;
+
 public class Controller {
 
+  Connection conn = null;
+  Statement stmt = null;
+
+  public void handleButtonAction(ActionEvent event) {
+    //Event Handler for "Add Product" Button
+    try {
+
+      stmt = conn.createStatement();
+      String sqlInsertProduct = "INSERT INTO Product(type, manufacturer, name) VALUES ( 'AUDIO', 'Apple', 'iPod' );";
+      ResultSet rs = stmt.executeQuery(sqlInsertProduct);
+      stmt.close();
+      conn.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  public void handleRecordButtonAction(ActionEvent actionEvent) {
+    //Event Handler for "Record Production" Button
+    System.out.println("Production Recorded.");
+  }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-
 /**
  * @author Jackson Turner
  */

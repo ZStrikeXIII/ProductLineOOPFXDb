aeec3c62b280dcdfff825a692fdfa15db2505c36
ZStrikeXIII <jacksonturner1999@gmail.com>
2019-11-07 16:25:55 -0500

    Version 10: Added Controller code for TableView.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,7 +9,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * The Controller class holds almost all of the code that will be executed during this project,
@@ -20,8 +28,45 @@ import javafx.scene.control.ComboBox;
 
 public class Controller {
 
+  @FXML
+  private TextField txtProdName;
+
+  @FXML
+  private TextField txtManu;
+
+  @FXML
+  private Button buttAddProd;
+
+  @FXML
+  private TextArea txtbxProdLog;
+
+  @FXML
+  private TableView<Product> tvProductLine;
+
+  @FXML
+  private ChoiceBox<ItemType> choiceItemType;
+
+  @FXML
+  private TableColumn<?, ?> colName;
+
+  @FXML
+  private TableColumn<?, ?> colManu;
+
+  @FXML
+  public TableColumn<?, ?> colType;
+
+  @FXML
+  private ListView<Product> listChooseProd;
+
+  @FXML
+  private Button buttRecordProd;
+
   Connection conn = null;
   Statement stmt = null;
+
+  private ObservableList<Product> productLine = FXCollections.observableArrayList();
+
+
 
   /**
    * The Event Handler for the "Add Product" Button.
@@ -39,10 +84,27 @@ public class Controller {
       stmt = conn.createStatement();
       String sqlInsertProduct =
           "INSERT INTO Product(type, manufacturer, name)" + " VALUES ( 'AUDIO', 'Apple', 'iPod' );";
+
+      ItemType type  = choiceItemType.getValue();
+
+      String typeString = type.code;
+
+      String name = txtProdName.getText();
+
+      String manufacturer = txtManu.getText();
+
+      Product newProduct = new Widget(name, manufacturer, type);
+
+      productLine.add(newProduct);
+
+      listChooseProd.getItems().add(newProduct);
+
       //ResultSet rs = stmt.executeQuery(sqlInsertProduct); //Bugged for "Dead Store to rs"
       stmt.executeUpdate(sqlInsertProduct);
       stmt.close();
       conn.close();
+
+
     } catch (SQLException e) {
       e.printStackTrace();
     }
@@ -65,5 +127,17 @@ public class Controller {
     chooseQuantity.setItems(numList);
     chooseQuantity.setEditable(true);
     chooseQuantity.getSelectionModel().selectFirst();
+
+    listChooseProd.getItems().addAll(productLine);
+
+    colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+    colManu.setCellValueFactory(new PropertyValueFactory<>("manufacturer"));
+
+    colType.setCellValueFactory(new PropertyValueFactory<>("type"));
+
+    tvProductLine.setItems(productLine);
+
+    choiceItemType.getItems().addAll(ItemType.values());
   }
 }
--- src/sample/ItemType.java
+++ src/sample/ItemType.java
@@ -1,16 +1,16 @@
 package sample;
 
 public enum ItemType {
-  AUDIO("Audio", "AU"),
-  VISUAL("Visual", "VI"),
-  AUDIO_MOBILE("AudioMobile", "AM"),
-  VISUAL_MOBILE("VisualMobile", "VM");
+  AUDIO("AU"),
+  VISUAL("VI"),
+  AUDIO_MOBILE("AM"),
+  VISUAL_MOBILE("VM");
 
-  public final String type;
+  //public final String type;
   public final String code;
 
-  ItemType(String type, String code) {
-    this.type = type;
+  ItemType(String code) {
+    //this.type = type;
     this.code = code;
   }
 }
